b00b5877ac820ee495e24733c4766114c27ad1f8
nan <2337286751@qq.com>
2022-08-13 03:26:55 +0800

    修改压缩文件位置

--- ci/script/deploy.sh
+++ ci/script/deploy.sh
@@ -3,6 +3,6 @@ chmod 600 ./ci/script/ci_rsa
 # 构建代码
 npx vite build --mode development
 # 压缩打包代码
-rm -rf /tmp/dist.zip
-zip -q -r /tmp/dist.zip ./dist
+rm -rf ./dist.zip
+zip -q -r ./dist.zip ./dist
 echo '代码包上传完成'
